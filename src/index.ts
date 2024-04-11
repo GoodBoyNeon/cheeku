e59226c5ab8b29ccc9628342ad56d6c671e2f853
GoodBoyNeon <goodboyneon03@gmail.com>
2024-04-11 09:22:29 +0545

    feat: annoucements sender

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { config } from './config';
 import { Client } from './lib';
+import announcement from '../announcement.json';
+import { writeFileSync } from 'fs';
 
 import './lib/controllers/bootstrapListeners';
+import { Channels } from 'lib/utils/constants';
+import { TextBasedChannel } from 'discord.js';
+import path from 'path';
 
 process.env.NODE_ENV ??= 'development';
 
@@ -12,7 +17,15 @@ void (async () => {
     partials: config.partials,
   });
 
-  client.init();
+  await client.init();
+
+  /* Announcements */
+
+  if (announcement.new) {
+    await ((await client.channels.fetch(Channels.noPingAnnouncements)) as TextBasedChannel).send(announcement.message);
+
+    writeFileSync(path.join(__dirname, '..', 'announcement.json'), JSON.stringify({ new: false, message: announcement.message }), 'utf-8');
+  }
 })();
 
 declare module 'bun' {
---
+++ src/lib/utils/constants.ts
@@ -0,0 +1,4 @@
+export const Channels = {
+  noPingAnnouncements: '977455361676156940',
+  // noPingAnnouncements: '952514063148146691',
+} as const;
