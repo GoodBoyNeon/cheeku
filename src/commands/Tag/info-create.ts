4abf4b753d6cd8c815cf962fed82d28e9ccb0004
GoodBoyNeon <goodboyneon03@gmail.com>
2024-04-18 21:17:25 +0545

    fix: info tag can only be created by regular members

--- src/commands/Tag/info-create.ts
+++ src/commands/Tag/info-create.ts
@@ -1,6 +1,7 @@
 import { Message } from 'discord.js';
 import { createTag } from '../../lib/modules/';
 import { Command, type Props } from '../../lib/structures';
+import { Constants } from '../../lib/utils';
 
 export default class TagCreateCommand extends Command {
   constructor(context: Props) {
@@ -11,6 +12,11 @@ export default class TagCreateCommand extends Command {
   }
 
   async run(message: Message, args: string[]) {
+    if (!message.member?.roles.cache.has(Constants.Roles.folksOfSnowdin)) {
+      await message.reply('You are not eligible to create an info tag.');
+      return;
+    }
+
     await createTag(message, args, 'Info');
   }
 }
--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -4,6 +4,11 @@ const Channels = {
   noPingAnnouncements: dev ? '952514063148146691' : '977455361676156940',
 } as const;
 
+const Roles = {
+  folksOfSnowdin: '743528017099489300',
+};
+
 export const Constants = {
   Channels,
+  Roles,
 } as const;
